c4dc6622be149cbc9a6ef3a2b0d6b25f5a85e9d7
IhorHodebskyi <i0962112822@gmail.com>
2024-01-02 23:53:02 +0200

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { ACTION } from 'next/dist/client/components/app-router-headers';
+import StatusLabel, { Status } from './components/active-label';
+
 export default function Home() {
   return (
     <main>
-      <h1>Home page</h1>
+      <h1 className="text-xl">Home page</h1>
+      <StatusLabel status={Status.Action}>Action</StatusLabel>
+      <StatusLabel status={Status.NotAction}>Not Action</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
     </main>
   );
 }
