071906c1a9a3c2af829a388dfb630e88e95fb351
IhorHodebskyi <i0962112822@gmail.com>
2024-01-03 23:00:41 +0200

    add

--- src/app/components/active-label.tsx
+++ src/app/components/active-label.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import clsx from 'clsx';
+
 export enum Status {
   Action = 'action',
   NotAction = 'notAction',
@@ -8,17 +10,24 @@ export enum Status {
 export interface StatusLabelProps {
   children: React.ReactNode;
   status: Status;
+  disablet?: boolean;
 }
 
-export default function StatusLabel({ children, status }: StatusLabelProps) {
+export default function StatusLabel({
+  children,
+  status,
+  disablet,
+}: StatusLabelProps) {
   return (
     <div
-      className={`inline-flex items-center py-1 px-3.5 rounded-3xl text-sm font-medium ${
-        (status === Status.Action && 'text-green-700 bg-green-100') ||
-        (status === Status.NotAction && 'text-red-700 bg-red-100') ||
-        (status === Status.Pending && 'text-orange-700 bg-orange-100') ||
-        (status === Status.Suspended && 'text-blue-700 bg-blue-100')
-      }`}
+      className={clsx(
+        'inline-flex items-center py-1 px-3.5 rounded-3xl text-sm font-medium',
+        status === Status.Action && 'text-green-700 bg-green-100',
+        status === Status.NotAction && 'text-red-700 bg-red-100',
+        status === Status.Pending && 'text-orange-700 bg-orange-100',
+        status === Status.Suspended && 'text-blue-700 bg-blue-100',
+        { ['opasity-75 cursor-not-allowed']: disablet }
+      )}
     >
       <div className="w-1 h-1 mr-2 rounded-full bg-current" />
       {children}
