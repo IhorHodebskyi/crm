89276490c7e9cdd95d2b3868aa0cabf59626202a
IhorHodebskyi <i0962112822@gmail.com>
2024-01-07 19:38:04 +0200

    add svg

--- src/app/components/sidebar-item.tsx
+++ src/app/components/sidebar-item.tsx
@@ -12,7 +12,7 @@ interface SidebarItemProps {
 const SidebarItem = ({ children, pathname, src, alt }: SidebarItemProps) => {
   return (
     <>
-      <a href={pathname} className="flex item-center h-9 mx1 gap-3.5">
+      <a href={pathname} className="flex item-center h-9 mx-1 gap-3.5">
         <Image className="ml-5" width={18} height={18} src={src} alt={alt} />
         <span className="font-medium text-zinc-50">{children}</span>
       </a>
--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Image from 'next/image';
 import SidebarItem from './sidebar-item';
 
@@ -7,15 +8,24 @@ const Sidebar = ({}: SidebarProps) => {
   return (
     <aside className="fixed top-0 left-0 z-40 w-60 h-screen">
       <div className="flex flex-col h-full overflow-y-auto bg-gray-900">
-        <Image className="" width={122} height={25} src="./icons/Group_1000004104.svg" alt="logo" />
+        <Image className="py-8 mb-11 mx-auto" width={122} height={25} src="/Group_1000004104.svg" alt="logo" />
+        <li className="space-y-7">
+          <ul>
+            <SidebarItem pathname="/dashboard" src="/Vector.svg" alt="Dashboard">
+              Dashboard
+            </SidebarItem>
+          </ul>
+          <ul>
+            <SidebarItem pathname="/companies" src="/Subtract.svg" alt="Companies">
+              Companies
+            </SidebarItem>
+          </ul>
+        </li>
+        <button className="flex items-center gap-2 p-6 mt-auto mx-auto">
+          <Image className="" width={18} height={18} src="/exit.svg" alt="" />
+          <span className="font-medium text-white">Exit</span>
+        </button>
       </div>
-
-      <ul className="space-y-7">
-        <SidebarItem pathname="/dashboard" src="" alt="">
-          Dashboard
-        </SidebarItem>
-      </ul>
-      <ul></ul>
     </aside>
   );
 };
